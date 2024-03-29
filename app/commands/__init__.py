c0f76849dcb7216018bcf50413aaac3a19748425
lovkyasushya <lb352@njit.edu>
2024-03-29 13:11:39 -0400

    Updated

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -9,18 +9,13 @@ class CommandHandler:
     def __init__(self):
         self.commands = {}
 
-    def register_command(self, command_name: str, command: Command):
-        self.commands[command_name] = command
+    def register_command(self, command_name: str, command_class):
+        self.commands[command_name] = command_class
 
-    def execute_command(self, command_name: str):
-        """ Look before you leap (LBYL) - Use when its less likely to work
+    def execute_command(self, command_name: str, *args):
         if command_name in self.commands:
-            self.commands[command_name].execute()
+            command_class = self.commands[command_name]
+            command = command_class(*args)
+            command.execute()
         else:
-            print(f"No such command: {command_name}")
-        """
-        """Easier to ask for forgiveness than permission (EAFP) - Use when its going to most likely work"""
-        try:
-            self.commands[command_name].execute()
-        except KeyError:
             print(f"No such command: {command_name}")
