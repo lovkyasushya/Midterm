bf4720db07de0d5b7845fd369db37d16a6f30e43
lovkyasushya <lb352@njit.edu>
2024-03-29 13:40:43 -0400

    done with basic structures

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,12 @@
 import pytest
-
 from app import App
 
 def test_app_get_environment_variable():
     app = App()
-#   Retrieve the current environment setting
-    current_env = app.getEnvironmentVariable('ENVIRONMENT')
+    # Retrieve the current environment setting
+    current_env = app.get_environment_variable('ENVIRONMENT')
     # Assert that the current environment is what you expect
     assert current_env in ['DEVELOPMENT', 'TESTING', 'PRODUCTION'], f"Invalid ENVIRONMENT: {current_env}"
-
-
 
 def test_app_start_exit_command(capfd, monkeypatch):
     """Test that the REPL exits correctly on 'exit' command."""
