54301739317086eae359c662550a3ab5d5b8261f
lovkyasushya <158232938+lovkyasushya@users.noreply.github.com>
2024-03-29 21:08:15 -0400

    Update __init__.py

--- app/plugins/subtraction/__init__.py
+++ app/plugins/subtraction/__init__.py
@@ -2,15 +2,11 @@ from app.commands import Command
 
 class SubtractionCommand(Command):
     def execute(self, args):
-        if len(args) >= 2:
-            try:
-                a = float(args[0])
-                b = float(args[1])
-                result = a - b
-                return result
-            except ValueError:
-                print("Invalid input. Please provide valid numbers.")
-                return None
+        if args:
+            a = float(args[0])
+            b = float(args[1])
+            result = a - b  # Perform subtraction
+            return result  # Return the result
         else:
-            print("Insufficient arguments. Please provide two numbers to subtract.")
-            return None
+            print("Nothing to subtract")
+            return None  # Return None if no arguments are provided
